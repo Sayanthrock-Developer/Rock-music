1b869e2bac794cb4674462146642866478133028
Sayanth Rock <sayanthsmeppayurvaliyaparambil@gmail.com>
2026-07-25 20:38:04 +0530

    chore: remove obsolete Compose scope alias

--- app/src/main/java/com/rockmusic/app/presentation/ComposeScopeAliases.kt
+++
@@ -1,6 +0,0 @@
-package com.rockmusic.app.presentation
-
-import androidx.compose.foundation.layout.ColumnScope
-
-/** Receiver used by the system-player panel content DSL. */
-internal typealias Column = ColumnScope
